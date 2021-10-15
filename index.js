ddc074eb5517617587fad5f42ac8b16d097a432c
horsemaker <yash_ghodekar@yahoo.in>
2021-10-15 23:12:59 +0530

    using template literals & innerHTML

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 
+let myLeads = []
+
+function renderLeads() {
+  var listItems = ''
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+        <li>
+            <a target="_blank" href="${myLeads[i]}">
+                ${myLeads[i]}
+            </a>
+        </li>
+    `
+  }
+  ulEl.innerHTML = listItems
+}
+
 function saveLead() {
   myLeads.push(inputEl.value)
+  renderLeads()
+  inputEl.value = ''
 }
 
 inputBtn.addEventListener('click', saveLead)
